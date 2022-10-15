d7cc32b090ae4a2c10e53637bac3678729d8b666
jhjulia <jibang@ucsd.edu>
2022-10-14 20:02:08 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -4,37 +4,28 @@ import java.util.ArrayList;
 
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
- 
+    public String handleRequest(URI url) {
+        ArrayList<String> search = new ArrayList<String>();
 
-    ArrayList<String> search = new ArrayList<String>();
-
-    public String handleRequest2(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("String list");
+            return String.format("String list" + search);
         } else if (url.getPath().equals("/search")) {
             url.getQuery();
-            for(int i = 0; i < search.size();){
+            return String.format(url.getQuery());
+            /*for(int i = 0; i < search.size();){
                 return String.format("The list is: ", search.get(i));
-            }
+            }*/
         } else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("s")) {
-                    return String.format("Added to a list", search.add(parameters[1]));
-                }
+                if (parameters[0].equals("count")) {
+                    
+                    return String.format("Number increased by %s! It's now %d", parameters[1], num);)
             }
             return "404 Not Found!";}
-        return null;
     }
+}
 
 
-
-    @Override
-    public String handleRequest(URI url) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    }
+   
