26600d75f554fc503105030f0c3b65d448e707ca
jhjulia <jibang@ucsd.edu>
2022-10-14 20:08:20 -0700

    Update Server.java

--- Server.java
+++ Server.java
@@ -26,7 +26,7 @@ class Handler implements URLHandler {
     }
 }
 
-class NumberServer {
+/*class NumberServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
@@ -37,4 +37,4 @@ class NumberServer {
 
         Server.start(port, new Handler());
     }
-}
+}*/
