1dd1f3482ade88e2fe63e7fd9293879f59bc45bc
jhjulia <jibang@ucsd.edu>
2022-10-28 19:19:37 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 class Handler implements URLHandler {
     public String handleRequest(URI url) {
         ArrayList<String> search = new ArrayList<String>();
-
+        String result = null;
         if (url.getPath().equals("/")) {
-            return String.format("String list" + search);
-        } else if (url.getPath().equals("/search")) {
+            return String.format("String list : " + search);
+        } else if (url.getPath().equals("/add")) {
             url.getQuery();
             return String.format(url.getQuery());
             /*for(int i = 0; i < search.size();){
@@ -17,15 +17,32 @@ class Handler implements URLHandler {
             }*/
         } else {
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            if (url.getPath().contains("search")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);)
+                for(int i = 0; i < parameters.length; i++){
+                    if(parameters[i] == url.getQuery()){
+                        result = parameters[i];
+                    }
+                }
+                }
             }
-            return "404 Not Found!";}
+            return result;
+            //return "404 Not Found!";
+        }
+    }
+
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
     }
 }
-
 
    
